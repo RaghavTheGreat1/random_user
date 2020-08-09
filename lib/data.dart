88abc7a62b8ddd17d07210d24fa27aaf923a424c
Keshav <fatesariakeshav@gamil.com>
2020-08-09 12:18:59 +0530

    added dta file

---
+++ lib/data.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class RandomUser {
+  var name;
+  var image;
+  var mail;
+  var number;
+  var password;
+  var location;
+  var dob;
+
+  Future<void> fetchData() async {}
+}
