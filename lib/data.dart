24ef16c31edd9a5a7712e614960f13743318d702
Keshav <fatesariakeshav@gamil.com>
2020-08-09 12:38:41 +0530

    .

--- lib/data.dart
+++ lib/data.dart
@@ -1,14 +1,19 @@
-import 'package:flutter/material.dart';
+
 import 'package:http/http.dart' as http;
 
 class RandomUser {
   var name;
-  var image;
+  var picture;
   var mail;
   var number;
   var password;
   var location;
   var dob;
 
-  Future<void> fetchData() async {}
+  Future<void> fetchData() async {
+    var response = await http.get('https://randomuser.me/api/');
+    print(response);
+  }
+
 }
+void main() => RandomUser()..fetchData();
